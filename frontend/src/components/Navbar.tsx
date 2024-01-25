4a44e22dc7c7e7f7a01aeb08085fb42a98298050
Alexandra Dernova <alexder@gmx.net>
2024-01-25 14:54:42 +0100

    cleaning NavBar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import {NavLink} from "react-router-dom";
 export default function NavBar() {
     return (
 
-        <div className="min-h-full">
+        <div>
             <StyledNav>
                 <NavContainer>
                     <FlexContainer>
